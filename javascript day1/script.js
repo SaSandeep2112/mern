f29a7844f0e3cec884cbfc3e09355ddb71291b8c
SaSandeep2112 <147911135+SaSandeep2112@users.noreply.github.com>
2024-04-04 16:31:59 +0530

    Add files via upload

---
+++ javascript day1/script.js
@@ -0,0 +1,248 @@
+// var maessage ='Hello world';
+// var age = 18;
+// age ="Done";
+// console.log(message);
+// console.log(age);
+// //variables
+// javascript is loosley typed,strictly typed, statically typed and dynamically typed?/.
+
+//hoisting
+
+// function env(){
+//     // console.log(message);i
+//     // var message = "Hello world!";
+//     // console.log(message);
+//     //  var message = "else";
+//     // console.log(message);
+// for(var i = 0; i<5; i++){
+//     console.log('--',i);
+// }
+// console.log(i);
+// }
+// env();
+// let message ="Hello";
+//  let message ="else"; {not allowed in let variable}
+//  message ="else";
+// console.log(message);
+// const message ="Hello";
+// message ="else";
+// message ="Hello";
+// console.log(message);
+// let age = 20;
+// console.log(typeof(age));
+//undefined va=s Null
+// let UserAge = 'abcd';
+// UserAge ='abbd';
+
+// console.log (typeof(UserAge));
+// let _ = 'abcd';
+// _ ='abbd';
+
+// console.log (typeof(_));
+// let arr = ['a','b,','c','d'];
+// arr[0] = 'abcd';
+// console.log(arr);
+// let str = "12";
+// let age = 20;
+// let ans = age-(-str);
+// console.log(ans);
+// let str ='12';
+// let p = parseInt(str);
+// let n = Number(str);
+// console.log(p,n);
+// let str ='12.12';
+// let p = parseInt(str);
+// let n = Number(str);
+// console.log(p,n);
+// let str ='12a';
+// let p = parseInt(str);
+// let n = Number(str);
+// console.log(p,n);
+// let a = '12';
+// let b = 12;
+// if(a==b){
+//     console.log('equal');
+// }
+// else{
+//     console.log('not equal');
+// }
+// if(a===b){
+//     console.log('equal yes');
+// }
+// else{
+//     console.log('not equal yes');
+// }
+// let a = ['a', 'b','c','d'];
+// let cpy = a;
+// if(a==cpy){
+//     console.log('equal');
+// }
+// else{
+//     console.log('not equal');
+// }
+// if(a===cpy){
+//     console.log('equal yes');
+// }
+// else{
+//     console.log('not equal yes');
+// }
+
+// const str = 'sandeep';
+
+// const name1 = 'MY Name is ';
+// const ans = name1+str;
+// console.log(ans);
+
+
+// const str = 'sandeep';
+
+// const name1 = 'MY Name is :';
+// const ans = `${name1} ${str}`;
+
+// console.log(ans);
+// let str = 'sahu';
+// const usrName = 'My Name is :';
+// const ans = `${usrName} ${str}`;
+// console.log(ans);
+
+
+// const a = 'my:';
+// const b = 'name';
+// const c = 'is';
+// const d =':';
+// const str = 'khan';
+// const ans = `${a} ${b} ${c} ${d} ${str}`;
+// console.log(ans);
+
+
+// function print(str){
+
+//     console.log(str);
+
+// }
+
+
+// print('hello world');
+
+
+// function print(str = 'nothing to print'){
+
+//     console.log(str);
+
+// }
+
+
+// print();
+// let a = 1;
+// if(a){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no');
+// }
+// function sum(a,b){
+//     if(a && b){
+//         console.log(a+b);
+//     }
+//     else if(a){
+//     console.log(a);
+// }
+// else{
+//     console.log('0');
+// }
+
+// sum(10);
+// let a = 0;            //0,false,null, undefined,empty string
+// if(a){
+//     console.log('yes');
+// }
+// else{
+//     console.log('no');
+// }
+
+//function declaration  (name)
+// function print(){
+//     console.log('--'); 
+// }
+// //function assignment  (anonymours)
+// const a = function print(){
+//     console.log('--');
+// }
+// const b = function(){
+//     console.log('--');
+// }
+// print();
+// a();
+// b();
+
+// function print(){
+//     console.log('--'); 
+// }
+// //function assignment  (anonymours)
+// const a = function print(){
+//     console.log('--');
+// }
+// const b = function(){
+//     console.log('--');
+// }
+// const c = () =>{
+//     console.log('--');
+// }
+// print();
+// a();
+// b();
+// c();
+// how many types to return value from arrow functions
+
+
+// let a =2;
+
+// switch(a){
+//     case 1:{
+//         console.log('one');
+        
+//     }
+//     case 2:{
+//         console.log('two');
+//     }
+//     case 3:{
+//         console.log('three');
+//     }
+//     default:{
+//         console.log('not 1,2,3');
+//     }
+// }
+
+// const obj = new Object();
+// // obj = {};.
+// obj[1] = 'one';
+// obj['name'] = 'sandeep';
+// obj[12] = 'twelve';
+// console.log(obj);
+
+// const obj = {
+// // obj = {};.
+// 1: 'one',
+// 'name': 'sandeep',
+// 12: 'twelve',
+// };
+
+// console.log(obj);
+
+// const obj = {
+//     // obj = {};.
+//     'firstName': 'sandeep',
+//     'lastName2': 'Sahu',
+//     'age': 20,
+//     'address': 'delhi',
+//     };
+
+//     const fullName = obj.firstName + ' ' + obj.lastName;
+//     const fullName2 = `${obj.firstName} ${obj.lastName};`
+    
+//     console.log(fullName);
+//     console.log(fullName2);
+
+
+// const x =prompt('please tell the propery to know');
+// console.log(obj[x]);
